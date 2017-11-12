4db0ad8ffc09740afb4423aaacc428d43c3c50d5
claire <clairemstolp@gmail.com>
2017-11-12 12:35:27 -0500

    Setup start scipt and heroku port

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
 
 app.use((req, res, next) =>{
   var now = new Date().toString();
@@ -21,13 +22,15 @@ app.use((req, res, next) =>{
 });
 
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
@@ -51,8 +54,8 @@ app.get('/bad', (req, res) => {
     errorMessage: 'Unable to handle request'
   });
 });
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port , () => {
+  console.log(`Server is up on port ${port}`);
 });
 
 
